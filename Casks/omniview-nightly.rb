bc207d3296a1063644f549737dace44f11b67818
Joshua Pare <jpare@mosaiclearning.com>
2026-02-24 15:56:41 -0600

    Add omniview-nightly cask

---
+++ Casks/omniview-nightly.rb
@@ -0,0 +1,23 @@
+cask "omniview-nightly" do
+  version "0.0.0-nightly.20260224"
+  sha256 "PLACEHOLDER"
+
+  url "https://github.com/omniviewdev/omniview/releases/download/nightly/Omniview_#{version}_darwin_universal.dmg"
+  name "Omniview Nightly"
+  desc "Open-source, pluggable IDE for Kubernetes and DevOps (nightly build)"
+  homepage "https://github.com/omniviewdev/omniview"
+
+  livecheck do
+    url "https://github.com/omniviewdev/omniview/releases/tag/nightly"
+    regex(/Omniview[._-]v?(\d+(?:\.\d+)*-nightly\.\d+)[._-]darwin/i)
+  end
+
+  app "Omniview.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.omniview.omniview",
+    "~/Library/Caches/com.omniview.omniview",
+    "~/Library/Preferences/com.omniview.omniview.plist",
+    "~/.omniview",
+  ]
+end
